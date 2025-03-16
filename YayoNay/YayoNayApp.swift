a634b3322f82005d3b93705ae7110cf9a345b759
Shahab <Shahabgeravesh@gmail.com>
2025-03-15 22:29:58 -0700

    Initial Commit

---
+++ YayoNay/YayoNayApp.swift
@@ -0,0 +1,17 @@
+//
+//  YayoNayApp.swift
+//  YayoNay
+//
+//  Created by Shahab Geravesh on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct YayoNayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YayoNayTests/YayoNayTests.swift
@@ -0,0 +1,16 @@
+//
+//  YayoNayTests.swift
+//  YayoNayTests
+//
+//  Created by Shahab Geravesh on 3/15/25.
+//
+
+import Testing
+
+struct YayoNayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
